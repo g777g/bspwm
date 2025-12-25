f051f8fa7a6c109d0405dc9529116091ca3f4ac4
g777g <76852433+g777g@users.noreply.github.com>
2025-12-25 10:51:23 +0200

    Add files via upload

---
+++ dotfiles/install_packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo pacman -S xorg xorg-xinit micro mousepad bspwm sxhkd polybar picom dunst dmenu brightnessctl nm-connection-editor networkmanager pulseaudio pipewire pipewire-pulse bluez bluez-utils blueman playerctl ttf-jetbrains-mono-nerd scrot zip unzip feh thunar thunar-archive-plugin xarchiver alacritty git udisks2 gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc polkit polkit-gnome geany
