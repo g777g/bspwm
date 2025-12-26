35c094f8dd767e9bf368fd8bc66d5a07351d02fa
g777g <76852433+g777g@users.noreply.github.com>
2025-12-26 08:50:13 +0200

    Update install_packages.sh

--- dotfiles/install_packages.sh
+++ dotfiles/install_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S xorg xorg-xinit micro mousepad bspwm sxhkd polybar picom dunst dmenu brightnessctl nm-connection-editor networkmanager pulseaudio pipewire pipewire-pulse bluez bluez-utils blueman playerctl ttf-jetbrains-mono-nerd scrot zip unzip feh thunar thunar-archive-plugin xarchiver alacritty git code udisks2 gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc polkit polkit-gnome geany geany-plugins
+sudo pacman -S xorg xorg-xinit fresh mousepad bspwm sxhkd polybar picom dunst dmenu brightnessctl nm-connection-editor networkmanager pulseaudio pipewire pipewire-pulse bluez bluez-utils blueman playerctl ttf-jetbrains-mono-nerd scrot zip unzip feh thunar thunar-archive-plugin xarchiver alacritty git code udisks2 gvfs gvfs-mtp gvfs-gphoto2 gvfs-afc polkit polkit-gnome geany geany-plugins
 yay -S --noconfirm google-chrome-stable
 
 # 2. Копирование Bash-конфига и xinitrc в домашнюю папку
